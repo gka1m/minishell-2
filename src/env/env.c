cdeb91535fe3e8a68b7911266f3d822d0ce74f46
theophanechan <theophanechan@yahoo.com>
2025-03-22 15:22:20 +0800

    norminette and forbidden functions check

--- src/env/env.c
+++ src/env/env.c
@@ -3,128 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkaim <gkaim@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zchan <zchan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:53:42 by theophane         #+#    #+#             */
-/*   Updated: 2025/03/07 14:16:12 by gkaim            ###   ########.fr       */
+/*   Updated: 2025/03/22 15:08:21 by zchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 #include "../../libft/libft.h"
 
-/* reference from header*/
-// typedef struct s_minishell
-// {
-//     char **env;          // Array of environment variables (similar to environ)
-//     int last_exit_code;  // Stores the exit code of the last executed command ($?)
-//     char cwd[PATH_MAX];  // Current working directory
-//     int interactive;     // Boolean: 1 for interactive mode, 0 for non-interactive
-// } t_minishell;
-
-// to init minishell structure
-// could also be in the wrong file, maybe minishell.c?
-/* t_minishell *init_minishell(char **envp)
+/*
+// (reference from header)
+// Array of environment variables (similar to environ)
+// Stores the exit code of the last executed command ($?)
+// Current working directory
+// Boolean: 1 for interactive mode, 0 for non-interactive
+typedef struct s_minishell
 {
-    t_minishell *minishell = malloc(sizeof(t_minishell));
-    if (!minishell)
-    {
-        perror("malloc error for minishell");
-        exit(EXIT_FAILURE);
-    }
+	char	**env;
+	int		last_exit_code;
+	char	cwd[PATH_MAX];
+	int		interactive;
+}			t_minishell;
+*/
 
-    // Copy the environment variables
-    int env_count = 0;
-    while (envp[env_count])
-        env_count++;
-    minishell->env = malloc((env_count + 1) * sizeof(char *));
-    if (!minishell->env)
-    {
-        perror("malloc error for env");
-        free(minishell);
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < env_count; i++)
-    {
-        minishell->env[i] = ft_strdup(envp[i]);
-    }
-    minishell->env[env_count] = NULL; // NULL-terminate the array
-
-    // Initialize other fields
-    minishell->last_exit_code = 0;
-    if (!getcwd(minishell->cwd, PATH_MAX))
-    {
-        perror("getcwd");
-        free(minishell->env);
-        free(minishell);
-        exit(EXIT_FAILURE);
-    }
-    minishell->interactive = isatty(STDIN_FILENO); // 1 if shell is interactive
-
-    return minishell;
-} */
-
-t_minishell *init_minishell(char **envp)
+t_minishell	*init_minishell(char **envp)
 {
-    t_minishell *minishell;
+	t_minishell	*minishell;
 
-    minishell = malloc(sizeof(t_minishell));
-    if (!minishell)
-    {
-        perror("malloc error for minishell");
-        exit(EXIT_FAILURE);
-    }
-    init_fields(minishell, envp);
-    minishell->interactive = isatty(STDIN_FILENO);
-    return (minishell);
+	minishell = malloc(sizeof(t_minishell));
+	if (!minishell)
+	{
+		perror("malloc error for minishell");
+		exit(EXIT_FAILURE);
+	}
+	init_fields(minishell, envp);
+	minishell->interactive = isatty(STDIN_FILENO);
+	return (minishell);
 }
 
-void    init_fields(t_minishell *minishell, char **envp)
+void	init_fields(t_minishell *minishell, char **envp)
 {
-    int env_count;
-    int i;
+	int	env_count;
+	int	i;
 
-    i = -1;
-    env_count = 0;
-    while (envp[env_count])
-        env_count++;
-    minishell->env = malloc((env_count + 1) * sizeof(char *));
-    if (!minishell->env)
-    {
-        perror("malloc error for env");
-        free(minishell);
-        exit(EXIT_FAILURE);
-    }
-    while (++i < env_count)
-        minishell->env[i] = ft_strdup(envp[i]);
-    minishell->env[env_count] = NULL;
-    minishell->last_exit_code = 0;
-    if (!getcwd(minishell->cwd, PATH_MAX))
-    {
-        perror("getcwd");
-        free(minishell->env);
-        free(minishell);
-        exit(EXIT_FAILURE);
-    }
+	i = -1;
+	env_count = 0;
+	while (envp[env_count])
+		env_count++;
+	minishell->env = malloc((env_count + 1) * sizeof(char *));
+	if (!minishell->env)
+	{
+		perror("malloc error for env");
+		free(minishell);
+		exit(EXIT_FAILURE);
+	}
+	while (++i < env_count)
+		minishell->env[i] = ft_strdup(envp[i]);
+	minishell->env[env_count] = NULL;
+	minishell->last_exit_code = 0;
+	if (!getcwd(minishell->cwd, PATH_MAX))
+	{
+		perror("getcwd");
+		free(minishell->env);
+		free(minishell);
+		exit(EXIT_FAILURE);
+	}
 }
 
 // function to free memory after exit
 // should be a utils somewhere
-void    free_minishell(t_minishell *minishell)
+// Free the environment variables then free struct itself
+void	free_minishell(t_minishell *minishell)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    // Free the environment variables
-    while (minishell->env[i])
-    {
-        free(minishell->env[i]);
-        i++;
-    }
-    free(minishell->env);
-
-    // Free the minishell struct itself
-    free(minishell);
+	i = 0;
+	while (minishell->env[i])
+	{
+		free(minishell->env[i]);
+		i++;
+	}
+	free(minishell->env);
+	free(minishell);
 }
 
 /* mains for testing the minishell environment*/
@@ -142,18 +104,60 @@ void    free_minishell(t_minishell *minishell)
 
 //         if (!fgets(input, sizeof(input), stdin)) { // Handle ctrl-D (EOF)
 //             if (minishell->interactive) printf("exit\n");
-//             break;
+//             break ;
 //         }
 
 //         // Process the input (parse, execute, etc.)
 //         // For now, simulate an exit command
-//         if (strcmp(input, "exit\n") == 0) break;
+//         if (strcmp(input, "exit\n") == 0) break ;
 
 //         // Example: Update last_exit_code (to mimic a successful command)
-//         minishell->last_exit_code = 0; 
+//         minishell->last_exit_code = 0;
 //     }
 
 //     // Cleanup
 //     free_minishell(minishell);
-//     return 0;
+//     return (0);
 // }
+
+// to init minishell structure
+// could also be in the wrong file, maybe minishell.c?
+/* t_minishell *init_minishell(char **envp)
+{
+	t_minishell *minishell = malloc(sizeof(t_minishell));
+	if (!minishell)
+	{
+		perror("malloc error for minishell");
+		exit(EXIT_FAILURE);
+	}
+
+	// Copy the environment variables
+	int env_count = 0;
+	while (envp[env_count])
+		env_count++;
+	minishell->env = malloc((env_count + 1) * sizeof(char *));
+	if (!minishell->env)
+	{
+		perror("malloc error for env");
+		free(minishell);
+		exit(EXIT_FAILURE);
+	}
+	for (int i = 0; i < env_count; i++)
+	{
+		minishell->env[i] = ft_strdup(envp[i]);
+	}
+	minishell->env[env_count] = NULL; // NULL-terminate the array
+
+	// Initialize other fields
+	minishell->last_exit_code = 0;
+	if (!getcwd(minishell->cwd, PATH_MAX))
+	{
+		perror("getcwd");
+		free(minishell->env);
+		free(minishell);
+		exit(EXIT_FAILURE);
+	}
+	minishell->interactive = isatty(STDIN_FILENO); // 1 if shell is interactive
+
+	return (minishell);
+} */
