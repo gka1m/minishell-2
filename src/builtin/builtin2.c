6ebab1dd16f2d249dc33b280794971b64e318234
theophanechan <theophanechan@yahoo.com>
2025-03-22 14:58:38 +0800

    norminette and forbidden function checks

--- src/builtin/builtin2.c
+++ src/builtin/builtin2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkaim <gkaim@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zchan <zchan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 13:58:25 by gkaim             #+#    #+#             */
-/*   Updated: 2025/03/07 16:07:18 by gkaim            ###   ########.fr       */
+/*   Updated: 2025/03/22 14:56:49 by zchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,85 @@
 
 // unset
 // Removes environment variables by name
-int builtin_unset(char **args, t_minishell *minishell)
+// Check if variable exists, if not (-1) print message
+// Remove the variable if it does
+int	builtin_unset(char **args, t_minishell *minishell)
 {
-    int i;
-    
-    i = 1;
-    if (!args[i])
-    {
-        printf("unset: missing argument\n");
-        return (-1);
-    }
+	int	i;
 
-    while (args[i])
-    {
-        remove_env_var(args[i], minishell);
-        i++;
-    }
-    return (0);
+	i = 1;
+	if (!args[i])
+	{
+		printf("unset: missing argument\n");
+		return (-1);
+	}
+	while (args[i])
+	{
+		if (find_env_var(args[i], minishell->env) == -1)
+			printf("unset: %s: no such variable\n", args[i]);
+		else
+			remove_env_var(args[i], minishell);
+		i++;
+	}
+	return (0);
 }
 
 // env
+// Check if environment is empty, print messasge
 // Prints all environment variables
-int builtin_env(t_minishell *minishell)
+int	builtin_env(t_minishell *minishell)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (minishell->env[i])
-    {
-        printf("%s\n", minishell->env[i]);
-        i++;
-    }
-    return (0);
+	if (!minishell->env || !minishell->env[0])
+	{
+		printf("env: environment is empty\n");
+		return (1);
+	}
+	i = 0;
+	while (minishell->env[i])
+	{
+		printf("%s\n", minishell->env[i]);
+		i++;
+	}
+	return (0);
 }
 
 // exit
 // Exits the shell with an optional exit code (args[1])
-int builtin_exit(char **args)
+// Use 255 as the error exit code if not numeric (as per bash)
+int	builtin_exit(char **args)
 {
-    int exit_code;
-    
-    exit_code = 0;
-    if (args[1])
-        exit_code = ft_atoi(args[1]);
-    exit(exit_code);
-    return (0); // This line will never be reached
+	int	exit_code;
+
+	exit_code = 0;
+	if (args[1])
+	{
+		if (!is_numeric(args[1]))
+		{
+			printf("exit: %s: numeric argument required\n", args[1]);
+			exit(255);
+		}
+		exit_code = ft_atoi(args[1]);
+	}
+	exit(exit_code);
+	return (0);
+}
+
+// Allow negative or positive signs
+// Hence ft_isdigit not enough
+static int	is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
--- src/builtin/builtin_utils.c
+++ src/builtin/builtin_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gkaim <gkaim@student.42.fr>                +#+  +:+       +#+        */
+/*   By: zchan <zchan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 12:52:21 by zchan             #+#    #+#             */
-/*   Updated: 2025/03/07 14:32:24 by gkaim            ###   ########.fr       */
+/*   Updated: 2025/03/22 14:33:16 by zchan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,91 +14,100 @@
 #include "../../libft/libft.h"
 
 // print env
-void    print_env_vars(char **env)
+void	print_env_vars(char **env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (env[i])
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
+	i = 0;
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
 
 // check env var is valid
-int is_valid_env_format(const char *arg)
+// Ensure the format contains '='
+int	is_valid_env_format(const char *arg)
 {
-    return (ft_strchr(arg, '=') != NULL); // Ensure the format contains '='
+	return (ft_strchr(arg, '=') != NULL);
 }
 
 // find env var
-int find_env_var(const char *var, char **env)
+// Return index of the variable
+// Return -1 if not found
+int	find_env_var(const char *var, char **env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (env[i])
-    {
-        if (ft_strncmp(env[i], var, ft_strlen(var)) == 0 &&
-            env[i][ft_strlen(var)] == '=')
-        {
-            return (i); // Return index of the variable
-        }
-        i++;
-    }
-    return (-1); // Not found
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], var, ft_strlen(var)) == 0
+			&& env[i][ft_strlen(var)] == '=')
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+// if -1
+// free the variable
+// and shift the remaining variables left using while loop
+// Ensure the last entry is NULL
+void	remove_env_var(const char *var, t_minishell *minishell)
+{
+	int	var_index;
+	int	i;
+
+	var_index = find_env_var(var, minishell->env);
+	if (var_index != -1)
+	{
+		free(minishell->env[var_index]);
+		i = var_index;
+		while (minishell->env[i])
+		{
+			minishell->env[i] = minishell->env[i + 1];
+			i++;
+		}
+		minishell->env[i] = NULL;
+	}
 }
 
 // add or update env var
 /* void    add_or_update_env_var(const char *new_var, t_minishell *minishell)
 {
-    int     i;
-    int     var_index;
-    int     count;
-    char    **new_env;
-    
-    var_index = find_env_var(new_var, minishell->env);
-    if (var_index != -1)
-    {
-        // Update existing variable
-        free(minishell->env[var_index]);
-        minishell->env[var_index] = ft_strdup(new_var);
-    }
-    // Add new variable
-    count = 0;
-    while (minishell->env[count])
-        count++;
-    new_env = malloc((count + 2) * sizeof(char *));
-    if (!new_env)
-    {
-        perror("malloc");
-        return ;
-    }
-    i = -1;
-    while (++i < count)
-        new_env[i] = minishell->env[i];
-    new_env[count] = ft_strdup(new_var);
-    new_env[count + 1] = NULL;
-    free(minishell->env);
-    minishell->env = new_env;
-} */
+	int		i;
+	int		var_index;
+	int		count;
+	char	**new_env;
+	int		var_index;
+	int		i;
 
-void remove_env_var(const char *var, t_minishell *minishell)
-{
-    int var_index;
-    int i;
-    
-    var_index = find_env_var(var, minishell->env);
-    if (var_index != -1)
-    {
-        // Free the variable and shift the remaining variables
-        free(minishell->env[var_index]);
-        i = var_index;
-        while (minishell->env[i])
-        {
-            minishell->env[i] = minishell->env[i + 1];
-            i++;
-        }
-    }
-}
+	var_index = find_env_var(new_var, minishell->env);
+	if (var_index != -1)
+	{
+		// Update existing variable
+		free(minishell->env[var_index]);
+		minishell->env[var_index] = ft_strdup(new_var);
+	}
+	// Add new variable
+	count = 0;
+	while (minishell->env[count])
+		count++;
+	new_env = malloc((count + 2) * sizeof(char *));
+	if (!new_env)
+	{
+		perror("malloc");
+		return ;
+	}
+	i = -1;
+	while (++i < count)
+		new_env[i] = minishell->env[i];
+	new_env[count] = ft_strdup(new_var);
+	new_env[count + 1] = NULL;
+	free(minishell->env);
+	minishell->env = new_env;
+} */
