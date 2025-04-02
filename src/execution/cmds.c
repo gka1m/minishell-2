b51a030254a09fbf1616f3e001938c09806cad14
Kai Ming Goh <kagoh@c1r3s4.42singapore.sg>
2025-04-02 17:28:58 +0800

    minor edits

--- src/execution/cmds.c
+++ src/execution/cmds.c
@@ -6,11 +6,32 @@
 /*   By: kagoh <kagoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 10:21:45 by kagoh             #+#    #+#             */
-/*   Updated: 2025/04/02 14:24:07 by kagoh            ###   ########.fr       */
+/*   Updated: 2025/04/02 15:58:22 by kagoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+int	execute_builtin(t_minishell *shell, char **args, int fd_out)
+{
+	if (!args[0])
+		return (0);
+	if (ft_strcmp(args[0], "cd") == 0)
+		return (b_cd(shell, &shell->env_list, args));
+	if (ft_strcmp(args[0], "echo") == 0)
+		return (b_echo(args));
+	if (ft_strcmp(args[0], "env") == 0)
+		return (b_env(shell->env_list));
+	if (ft_strcmp(args[0], "exit") == 0)
+		return (b_exit(shell, args));
+	if (ft_strcmp(args[0], "export") == 0)
+		return (bi_export(shell, args, fd_out));
+	if (ft_strcmp(args[0], "unset") == 0)
+		return (b_unset(shell->env_list, args));
+	if (ft_strcmp(args[0], "pwd") == 0)
+		return (b_pwd());
+	return (0);
+}
 
 void    parent_process(int pid, t_minishell *shell)
 {
